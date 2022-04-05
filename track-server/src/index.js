f1b3a27e9ff264d111cea3fc4093bfd9df8a37ac
Lim Chin Yan <chinyan_lim@ecics.com.sg>
2022-04-05 18:27:40 +0800

    second commit

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,13 +1,15 @@
 require('./models/User')
+require('./models/Track')
 const express = require('express')
-const req = require('express/lib/request')
 const mongoose = require('mongoose')
 const authRoutes = require('./routes/authRoutes')
+const trackRoutes = require('./routes/trackRoutes')
 const requireAuth = require('./middlewares/requireAuth')
 
 const app = express()
 app.use(express.json())
 app.use(authRoutes)
+app.use(trackRoutes)
 
 app.get('/', requireAuth, (req, res) => {
     // only allow a user to access this route if they have a valid jwt
